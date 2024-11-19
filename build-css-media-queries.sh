4db4bcfc52f7cd2f7958c46f1a1d6af11162a3b4
Márton Lente <lentemarton@gmail.com>
2024-11-19 17:36:42 +0100

    Fix make script bash helper built css valid by adding helpers

--- build-css-media-queries.sh
+++ build-css-media-queries.sh
@@ -11,6 +11,18 @@ dir_src="build"
 file_orig="$dir_src/baseutil-utilities.css"
 content_orig=$(cat "$file_orig")
 
+# Find the line number of the comment helper
+line_num=$(grep -n -m 1 -F -x "/*! Helper build-css-media-queries.sh */" "$file_orig" | cut -d: -f1)
+
+if [[ -z "$line_num" ]]; then
+  echo "\"Helper build-css-media-queries.sh\" comment helper not found"
+  exit 1
+fi
+
+# Extract content after and before the comment helper
+comment_before=$(head -n "$line_num" "$file_orig")
+comment_after=$(tail -n +"$((line_num + 1))" "$file_orig")
+
 # Iterate over each breakpoint
 for breakpoint in "${!breakpoints[@]}"; do
   max_width=${breakpoints[$breakpoint]}
@@ -18,9 +30,10 @@ for breakpoint in "${!breakpoints[@]}"; do
   # Create a file variant for the breakpoint
   file_variant="$dir_build/baseutil-utilities-$breakpoint.css"
 
+  echo "$comment_before" > "$file_variant"
   echo "@media (max-width: ${max_width}px) {" >> "$file_variant"
   
-  # Read each line from content orig and add the suffix for breakpoint
+  # Read each line from comment_after and add the suffix for the breakpoint
   while read -r line; do
     if [[ $line == .* ]]; then
       # Append the suffix breakpoint to the selector
@@ -29,7 +42,7 @@ for breakpoint in "${!breakpoints[@]}"; do
       line_mod=$line
     fi
     echo "$line_mod" >> "$file_variant"
-  done <<< "$content_orig"
+  done <<< "$comment_after"
   
   echo "}" >> "$file_variant"
 done
